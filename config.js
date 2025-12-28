4042e322afdbbd81f85cdf5b8881cb58d56b3a7c
StixOfficial <evanjameswilliams13@gmail.com>
2025-12-28 23:47:04 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,38 @@
 module.exports = {
   embedColor: "#b7ff00",
 
+  staffRole: "1447572189451653120",
+
   panel: {
     title: "Support Center",
     description:
-      "We aim to provide the best support we can.\nPlease ensure you open your inquiry in the correct category below.",
+      "**We aim to provide the best support we can. Please ensure you open your inquiry in the correct category below.**\n\n" +
+      "🛠️ **Script Support**\n" +
+      "This category is for members with the Customer role who are seeking support for a paid resource. We do not offer ticket support for our free resources.\n\n" +
+      "✅ **Claim Role**\n" +
+      "If you purchased one of our resources and did not receive your Customer role, open a ticket here & we will get you sorted!\n\n" +
+      "✉️ **Other**\n" +
+      "Won a giveaway? Have some questions before you purchase? Need something else? Open a ticket in this category for general correspondence."
   },
 
   categories: [
     {
       label: "Script Support",
-      description: "Paid resource support",
       emoji: "🛠️",
       value: "support",
-      categoryId: "1447572284150780028",
+      categoryId: "1447572284150780028"
     },
     {
       label: "Claim Role",
-      description: "Redeem your Customer role",
       emoji: "✅",
       value: "role",
-      categoryId: "1447572274415931532",
+      categoryId: "1447572274415931532"
     },
     {
       label: "Other",
-      description: "General questions",
       emoji: "✉️",
       value: "other",
-      categoryId: "1447572314253299803",
-    },
-  ],
+      categoryId: "1447572314253299803"
+    }
+  ]
 };
