ac2b882cd8b48a42558f89988981b1b68db1ca83
StixOfficial <evanjameswilliams13@gmail.com>
2025-12-29 23:21:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,36 +4,37 @@ module.exports = {
   staffRole: "1447572189451653120",
 
   panel: {
-    title: "<:sada:1455336425989083260> Support Center",
+    title: "<:fuze:1455337674369138761> Support Center",
     description:
       "**We aim to provide the best support we can. Please ensure you open your inquiry in the correct category below.**\n\n" +
-      "🛠️ **Script Support**\n" +
+      "<:EventTeam:1301391438445281280> **Script Support**\n" +
       "This category is for members with the Customer role who are seeking support for a paid resource. We do not offer ticket support for our free resources.\n\n" +
-      "✅ **Claim Role**\n" +
+      "<:52192membergreen:1455338571626975263> **Claim Role**\n" +
       "If you purchased one of our resources and did not receive your Customer role, open a ticket here & we will get you sorted!\n\n" +
-      "✉️ **Other**\n" +
+      "<:771156giveaway:1455339784213102726> **Other**\n" +
       "Won a giveaway? Have some questions before you purchase? Need something else? Open a ticket in this category for general correspondence."
   },
 
   categories: [
     {
       label: "Script Support",
-      emoji: "🛠️",
+      emoji: "<:EventTeam:1301391438445281280>",
       value: "support",
       categoryId: "1447572284150780028"
     },
     {
       label: "Claim Role",
-      emoji: "✅",
+      emoji: "<:52192membergreen:1455338571626975263>",
       value: "role",
       categoryId: "1447572274415931532"
     },
     {
       label: "Other",
-      emoji: "✉️",
+      emoji: "<:771156giveaway:1455339784213102726>",
       value: "other",
       categoryId: "1447572314253299803"
     }
   ]
 };
+
 
